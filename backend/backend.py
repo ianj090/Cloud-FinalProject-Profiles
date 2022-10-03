759bbbe23d31516a3e0ae526b5b4ad7c9e10d230
DavidCorzo <48890345+DavidCorzo@users.noreply.github.com>
2022-10-03 09:54:03 -0600

    added documentation

--- backend/backend.py
+++ backend/backend.py
@@ -1,19 +1,19 @@
 from flask import Flask, request
-import pymongo
+import pymongo # Mongo DB client
 from hashlib import md5 # Encoder for password
-# import os # random string method
 
 # Set workspace values / app config
-DATABASE = 'profiles.db' # Database name for sqlite3
 DEBUG = True # debug flag to print error information
 
 app = Flask(__name__)
 app.config.from_object(__name__) # loads workspace values
 
+# Connects to MongoDB, creates database 'flask_db' and collection 'users'
 client = pymongo.MongoClient("mongodb://host.docker.internal:27017/")
 db = client["flask_db"]
 users = db["users"]
 
+# Global variable to keep logged in user information
 global session
 session = {'logged_in':False, 'username':''}
 
@@ -29,28 +29,31 @@ def get_current_user():
     else:
         raise RuntimeError
 
-#### From route /
+# Resets session variable (for logout)
 def reset_session():
     session['logged_in'] = False
     session['username'] = ''
 
-# Redirects to '/login' path
+# Handles Frontend GET or POST login
 @app.route('/login', methods=["POST"])
 def login():
-    reset_session()
+    reset_session() # Login screen always resets session (assumes logout)
     payload = request.json
+    # Checks if a POST request was made to Frontend Login, if not then it skips (this route handles both GET and POST for Frontend)
     if payload["method"] == "POST":
         try:
+            # Checks if user exists and if password is correct
             try:
                 pw_hash = md5(payload["data"]['inputPassword'].encode('utf-8')).hexdigest()
                 user = users.find_one({'username': payload["data"]['inputUsername'], 'password': pw_hash})
                 auth_user(user)
                 return 'PROFILE'
             except:
+                # Tries to add user to database (signup), if it fails then the user exists but the password is wrong
                 try:
                     user = users.find_one({'username': payload["data"]['inputUsername']})
                     if user:
-                        raise RuntimeError
+                        raise RuntimeError # Username is already taken
                     user_dict = {
                         "username": payload["data"]['inputUsername'],
                         "password": md5((payload["data"]['inputPassword']).encode('utf-8')).hexdigest(),
@@ -72,17 +75,19 @@ def login():
                     users.insert_one(user_dict)
                     auth_user(user_dict)
                     return 'PROFILE'
+                # User exists but password is incorrect
                 except:
                     return 'TAKEN'
         except:
             return 'FAILURE'
     return 'LOGIN'
 
+# Handles Frontend POST profile
 @app.route('/profile', methods=["POST"])
 def homepage():
     payload = request.json
     user = users.find_one({'username': payload["data"]["searchUsername"]})
-    del user['_id']
+    del user['_id'] # Added by mongoDB, unnecessary for this app
     # Checks if user is the current session user
     if user["username"] == session['username']:
         current = True
@@ -91,6 +96,7 @@ def homepage():
     user["current"] = current
     return user
 
+# Handles any Frontend route where it requires checking who the current logged in user is
 @app.route('/currentUser')
 def currentUser():
     try:
@@ -100,10 +106,12 @@ def currentUser():
     except:
         return 'FAILURE'
 
+# Handles Frotend POST edit
 @app.route('/edit', methods=['POST'])
 def edit():
     payload = request.json
     current_user = get_current_user()
+    # profilepic is a url and often gives errors with the HTML, this value is not shown in the form so if it is left blank, it should not overwrite the previous url.
     if (payload["data"]['editProfilePic'] != ""):
         users.update_one({'username': current_user["username"]}, {"$set": {
             "profilepic": payload["data"]['editProfilePic'],
@@ -137,6 +145,7 @@ def edit():
         }})
     return "SUCCESS"
 
+# Handles Frotend POST editbg
 @app.route('/editbg', methods=['POST'])
 def editbg():
     payload = request.json
@@ -146,12 +155,11 @@ def editbg():
     }})
     return "SUCCESS"
     
-
+# Handles Frontend delete
 @app.route('/delete')
 def delete():
     users.delete_one({'username': session["username"]})
-    session['logged_in'] = False
-    session['username'] = ''
+    reset_session() # Resets the session just in case, still sends to Login so the user should be logged out anyway
     return 'SUCCESS'
 
 
